aee65939ad894676d3dd27f8fe491023461d8108
Adrian Dorey <adorey91@gmail.com>
2024-12-04 12:05:51 -0400

    Added controls for boost

--- Assets/Scripts/UI/UI_Collection.cs
+++ Assets/Scripts/UI/UI_Collection.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class UI_Collection : MonoBehaviour
 {
     [SerializeField] private TMP_Text collectionText;
+    [SerializeField] private TMP_Text energyCollection;
 
     public void UpdateCollectionText(string collectionAmount)
     {
         collectionText.text = collectionAmount;
+    }
+
+    public void UpdateEnergyText(string collectionAmount)
+    {
+        energyCollection.text = collectionAmount;
     }
 }
